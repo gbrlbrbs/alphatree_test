446997531970333255cc0bf9dd44137efb352f6f
Gabriel Martinz <gabrielbarbosamartinz_@outlook.com>
2021-12-12 23:51:21 -0300

    data etl and changes to file reader

---
+++ alphatree_test/data_loader.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from alphatree_test.file_reader import FileReader
+from typing import Iterable, Callable
+from functools import reduce
+
+
+class DataPipeline():
+    def __init__(self, transformations: Iterable[Callable], combinator: Callable, columns_function: Iterable[Iterable[str]]) -> None:
+        self.file_reader = FileReader()
+        self.transformations = transformations
+        self.combinator = combinator
+        self.dataframes = []
+        self.columns_function = columns_function
+
+    def __get_data(self):
+        files = self.file_reader.list_staging_files()
+        self.dataframes = list(map(pd.read_csv, files))
+    
+    def __transform_data(self):
+        for df in self.dataframes:
+            dfs = [df[columns].apply(f) for f, columns in zip(self.transformations, self.columns_function)]
+            df = reduce(self.combinator, dfs)
+
+    def __load_data(self):
+        _ = list(map(self.file_reader.create_consolidated_file, self.dataframes))
+
+    def start_pipeline(self):
+        self.__get_data()
+        self.__transform_data()
+        self.__load_data()
--- alphatree_test/file_reader.py
+++ alphatree_test/file_reader.py
@@ -2,6 +2,9 @@ import alphatree_test.constants as c
 import datetime as dt
 from pathlib import Path
 import pandas as pd
+from os.path import getmtime
+from time import sleep
+
 
 class FileReader():
     def __init__(self) -> None:
@@ -13,14 +16,17 @@ class FileReader():
         file.unlink(missing_ok=True)
 
     def list_staging_files(self):
-        return self.staging_path.glob('*.csv')
+        files = sorted(self.staging_path.glob('*.csv'), key=getmtime, reverse=True)
+        return files
 
     def create_consolidated_file(self, df: pd.DataFrame):
-        formatted_time = self.current_time.strftime('%Y-%m-%d_%H-%M-%S')
+        sleep(1)
+        current_time = dt.datetime.now()
+        formatted_time = current_time.strftime('%Y_%m_%d_%H_%M_%S')
         df.to_csv(self.processed_path / f'processed_{formatted_time}.csv')
 
     def remove_staging_files(self):
         staging_files = self.list_staging_files()
-        _ = map(self.__remove_file, staging_files)
+        _ = list(map(self.__remove_file, staging_files))
 
         
